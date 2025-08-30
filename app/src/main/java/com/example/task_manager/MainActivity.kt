69a556782df81730e7d539a91844197a3b886ea6
Gabriel Cuba <gabrielcuba2017@gmail.com>
2025-08-29 23:22:35 -0500

    Task manager initialization

--- app/src/main/java/com/example/task_manager/MainActivity.kt
+++ app/src/main/java/com/example/task_manager/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.task_manager.ui.theme.TaskmanagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +29,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskmanagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppScreenContent()
                 }
             }
         }
@@ -31,17 +38,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CheckImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Image(
+        painter = image,
+        contentDescription = "Task Completed",
         modifier = modifier
     )
+}
+
+@Composable
+fun MessageText(title: String, subtitle: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+    )
+    Text(
+        text = subtitle,
+        fontSize = 16.sp,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun AppScreenContent(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize())
+    {
+        CheckImage()
+        MessageText(stringResource(R.string.title_string), stringResource(R.string.subtitle_string))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskmanagerTheme {
-        Greeting("Android")
+        AppScreenContent()
     }
 }
